6d0df7005d6528f47d2e7977db6976fd0fde572b
bachidze <bachidze90@gmail.com>
2024-03-15 17:56:19 +0400

    Feat(add):speakers/earphnes page (mobile)

--- src/app/(pages)/earphones/page.tsx
+++ src/app/(pages)/earphones/page.tsx
@@ -1,11 +1,179 @@
-import React from 'react'
-interface Params {
-  params:{
-    id:string
-  }
-}
-export default function EarPhone({params}:Params) {
+import React from "react";
+import data from "../../../data.json";
+import Link from "next/link";
+import Image from "next/image";
+export default function EarPhone() {
   return (
-    <div>{params.id}</div>
-  )
+    <>
+      <main className="flex justify-center flex-col items-center">
+        <section className="bg-[#000000] w-full">
+          <div className="flex justify-center pt-8 pb-8">
+            <h1 className="text-white text-[28px] leading-[38.25px] font-bold tracking-[2px]">
+              EARPHONES
+            </h1>
+          </div>
+        </section>
+        <section className="pl-6 pr-6 mt-16 flex flex-col justify-center items-center">
+          <div className="mt-8">
+            <Image
+              className="rounded-xl"
+              alt="headphone"
+              src={data[0].categoryImage.mobile}
+              width={327}
+              height={353}
+            />
+          </div>
+
+          <div className="mt-8 mb-[120px]">
+            <div className=" flex justify-center flex-col items-center">
+              <h1 className="text-[14px] leading-[19px] tracking-[10px] text-center font-normal pb-4 opacity-50 text-[#D87D4A]">
+                NEW PRODUCT
+              </h1>
+              <h1 className="text-[28px] leading-[38.25px] font-bold tracking-[1px] text-center pb-6">
+                {data[0].name}
+              </h1>
+              <p className="text-[15px] text-center leading-[25px] font-normal opacity-50 w-[99%] pb-7">
+                Tailor your listening experience with bespoke dynamic drivers
+                from the new YX1 Wireless Earphones. Enjoy incredible
+                high-fidelity sound even in noisy environments with its active
+                noise cancellation feature.
+              </p>
+              <div className="bg-[#D87D4A]  tracking-[2px] w-[160px] flex items-center justify-center pt-[15px] pb-[15px] cursor-pointer">
+                <Link
+                  href={""}
+                  className="text-[13px] leading-[17.76px] font-bold tracking-[1px] text-white"
+                >
+                  SEE PRODUCT
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="flex flex-col gap-20 mt-24 text-center -z-20">
+          <div className="relative w-[327px] h-[165px] bg-[#F1F1F1] flex  flex-col justify-between pb-[22px] rounded-xl">
+            <div className="relative flex justify-center bottom-14">
+              <Image
+                alt="photo"
+                src={"/HeadPhoneHeader.svg"}
+                width={79}
+                height={104}
+              />
+            </div>
+            <div className="pb-[7px] relative bottom-4 text-[#000000]">
+              <Link
+                href={"/headphones"}
+                className="text-[15px] font-bold leading-5 tracking-widest"
+              >
+                HEADPHONES
+              </Link>
+            </div>
+            <div className="flex justify-center relative bottom-4 gap-2 text-[#000000]">
+              <Link
+                href={"/headphones"}
+                className="opacity-50 text-[13px] tracking-widest font-bold leading-[17px]"
+              >
+                SHOP
+              </Link>
+              <Image
+                alt="Arrow"
+                src={"/OrangeImg.svg"}
+                width={6.5}
+                height={10}
+              />
+            </div>
+          </div>
+
+          <div className="relative w-[327px] h-[165px] bg-[#F1F1F1] flex  flex-col justify-between pb-[22px] rounded-xl">
+            <div className="relative flex justify-center bottom-14">
+              <Image
+                alt="image"
+                src={"/SpeakerHeader.svg"}
+                width={79}
+                height={104}
+              />
+            </div>
+            <div className="pb-[7px] relative bottom-4 text-[#000000]">
+              <Link
+                href={"/speakers"}
+                className="text-[15px] font-bold leading-5 tracking-widest"
+              >
+                SPEAKERS
+              </Link>
+            </div>
+            <div className="flex justify-center relative bottom-4 gap-2 text-[#000000]">
+              <Link
+                href={"/headphones"}
+                className="opacity-50 text-[13px] tracking-widest font-bold leading-[17px]"
+              >
+                SHOP
+              </Link>
+              <Image
+                alt="Arrow"
+                src={"/OrangeImg.svg"}
+                width={6.5}
+                height={10}
+              />
+            </div>
+          </div>
+
+          <div className="relative w-[327px] h-[165px] bg-[#F1F1F1] flex  flex-col justify-between pb-[22px] rounded-xl">
+            <div className="relative flex justify-center bottom-12">
+              <Image
+                alt="image"
+                src={"/earPhoneHeader.svg"}
+                width={79}
+                height={104}
+              />
+            </div>
+            <div className="pb-[7px] relative bottom-5 text-[#000000]">
+              <Link
+                href={"/earphones"}
+                className="text-[15px] font-bold leading-5 tracking-widest"
+              >
+                EARPHONES
+              </Link>
+            </div>
+            <div className="flex justify-center relative bottom-4 gap-2 text-[#000000]">
+              <Link
+                href={"/headphones"}
+                className="opacity-50 text-[13px] tracking-widest font-bold leading-[17px]"
+              >
+                SHOP
+              </Link>
+              <Image
+                alt="Arrow"
+                src={"/OrangeImg.svg"}
+                width={6.5}
+                height={10}
+              />
+            </div>
+          </div>
+        </section>
+        <section className="flex justify-center items-center flex-col mt-[120px]">
+          <Image
+            className="rounded-xl"
+            alt="men"
+            src={data[3].gallery.first.mobile}
+            width={327}
+            height={200}
+          />
+          <div className="text-center mt-10">
+            <h1 className="text-[34px] leading-[38.25px] font-bold tracking-[1px] pb-8">
+              Bringing you the <span className="text-[#D87D4A]">best</span>{" "}
+              audio gear
+            </h1>
+            <p className="text-[15px] leading-[25px] font-normal w-[80%] m-auto opacity-65">
+              Located at the heart of New York City, Audiophile is the premier
+              store for high end headphones, earphones, speakers, and audio
+              accessories. We have a large showroom and luxury demonstration
+              rooms available for you to browse and experience a wide range of
+              our products. Stop by our store to meet some of the fantastic
+              people who make Audiophile the best place to buy your portable
+              audio equipment.
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
--- src/app/(pages)/speakers/page.tsx
+++ src/app/(pages)/speakers/page.tsx
@@ -1,7 +1,196 @@
-import React from 'react'
-
+import React from "react";
+import data from "../../../data.json";
+import Link from "next/link";
+import Image from "next/image";
 export default function Speakers() {
   return (
-    <div>Speakers</div>
-  )
+    <main className="flex justify-center flex-col items-center">
+      <section className="bg-[#000000] w-full">
+        <div className="flex justify-center pt-8 pb-8">
+          <h1 className="text-white text-[28px] leading-[38.25px] font-bold tracking-[2px]">
+            SPEAKERS
+          </h1>
+        </div>
+      </section>
+      <section className="pl-6 pr-6 mt-16 flex flex-col justify-center items-center">
+        <div className="mt-8">
+          <Image
+            className="rounded-xl"
+            alt="headphone"
+            src={data[5].categoryImage.mobile}
+            width={327}
+            height={353}
+          />
+        </div>
+
+        <div className="mt-8 -z-10">
+          <div className=" flex justify-center flex-col items-center">
+            <h1 className="text-[14px] leading-[19px] tracking-[10px] text-center font-normal pb-4 opacity-50 text-[#D87D4A]">
+              NEW PRODUCT
+            </h1>
+            <h1 className="text-[28px] leading-[38.25px] font-bold tracking-[1px] text-center pb-6">
+              {data[5].name}
+            </h1>
+            <p className="text-[15px] text-center leading-[25px] font-normal opacity-50 w-[99%] pb-7">
+              Upgrade your sound system with the all new ZX9 active speaker.
+              It&apos;s a bookshelf speaker system that offers truly wireless
+              connectivity -- creating new possibilities for more pleasing and
+              practical audio setups.
+            </p>
+            <div className="bg-[#D87D4A]  tracking-[2px] w-[160px] flex items-center justify-center pt-[15px] pb-[15px] cursor-pointer">
+              <Link
+                href={""}
+                className="text-[13px] leading-[17.76px] font-bold tracking-[1px] text-white"
+              >
+                SEE PRODUCT
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-[120px]">
+          <Image
+            className="rounded-xl"
+            alt="headphone"
+            src={data[4].categoryImage.mobile}
+            width={327}
+            height={353}
+          />
+        </div>
+
+        <div className="mt-8">
+          <div className=" flex justify-center flex-col items-center">
+            <h1 className="text-[14px] leading-[19px] tracking-[10px] text-center font-normal pb-4 opacity-50 text-[#D87D4A]">
+              NEW PRODUCT
+            </h1>
+            <h1 className="text-[28px] leading-[38.25px] font-bold tracking-[1px] text-center pb-6">
+              {data[4].name}
+            </h1>
+            <p className="text-[15px] text-center leading-[25px] font-normal opacity-50 w-[99%] pb-7">
+              Stream high quality sound wirelessly with minimal loss. The ZX7
+              bookshelf speaker uses high-end audiophile components that
+              represents the top of the line powered speakers for home or studio
+              use.
+            </p>
+            <div className="bg-[#D87D4A]  tracking-[2px] w-[160px] flex items-center justify-center pt-[15px] pb-[15px] cursor-pointer mb-[100px]">
+              <Link
+                href={""}
+                className="text-[13px] leading-[17.76px] font-bold tracking-[1px] text-white"
+              >
+                SEE PRODUCT
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col gap-20 mt-24 text-center -z-20">
+        <div className="relative w-[327px] h-[165px] bg-[#F1F1F1] flex  flex-col justify-between pb-[22px] rounded-xl">
+          <div className="relative flex justify-center bottom-14">
+            <Image
+              alt="photo"
+              src={"/HeadPhoneHeader.svg"}
+              width={79}
+              height={104}
+            />
+          </div>
+          <div className="pb-[7px] relative bottom-4 text-[#000000]">
+            <Link
+              href={"/headphones"}
+              className="text-[15px] font-bold leading-5 tracking-widest"
+            >
+              HEADPHONES
+            </Link>
+          </div>
+          <div className="flex justify-center relative bottom-4 gap-2 text-[#000000]">
+            <Link
+              href={"/headphones"}
+              className="opacity-50 text-[13px] tracking-widest font-bold leading-[17px]"
+            >
+              SHOP
+            </Link>
+            <Image alt="Arrow" src={"/OrangeImg.svg"} width={6.5} height={10} />
+          </div>
+        </div>
+
+        <div className="relative w-[327px] h-[165px] bg-[#F1F1F1] flex  flex-col justify-between pb-[22px] rounded-xl">
+          <div className="relative flex justify-center bottom-14">
+            <Image
+              alt="image"
+              src={"/SpeakerHeader.svg"}
+              width={79}
+              height={104}
+            />
+          </div>
+          <div className="pb-[7px] relative bottom-4 text-[#000000]">
+            <Link
+              href={"/speakers"}
+              className="text-[15px] font-bold leading-5 tracking-widest"
+            >
+              SPEAKERS
+            </Link>
+          </div>
+          <div className="flex justify-center relative bottom-4 gap-2 text-[#000000]">
+            <Link
+              href={"/headphones"}
+              className="opacity-50 text-[13px] tracking-widest font-bold leading-[17px]"
+            >
+              SHOP
+            </Link>
+            <Image alt="Arrow" src={"/OrangeImg.svg"} width={6.5} height={10} />
+          </div>
+        </div>
+
+        <div className="relative w-[327px] h-[165px] bg-[#F1F1F1] flex  flex-col justify-between pb-[22px] rounded-xl">
+          <div className="relative flex justify-center bottom-12">
+            <Image
+              alt="image"
+              src={"/earPhoneHeader.svg"}
+              width={79}
+              height={104}
+            />
+          </div>
+          <div className="pb-[7px] relative bottom-5 text-[#000000]">
+            <Link
+              href={"/earphones"}
+              className="text-[15px] font-bold leading-5 tracking-widest"
+            >
+              EARPHONES
+            </Link>
+          </div>
+          <div className="flex justify-center relative bottom-4 gap-2 text-[#000000]">
+            <Link
+              href={"/headphones"}
+              className="opacity-50 text-[13px] tracking-widest font-bold leading-[17px]"
+            >
+              SHOP
+            </Link>
+            <Image alt="Arrow" src={"/OrangeImg.svg"} width={6.5} height={10} />
+          </div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center flex-col mt-[120px]">
+        <Image
+          className="rounded-xl"
+          alt="men"
+          src={data[3].gallery.first.mobile}
+          width={327}
+          height={200}
+        />
+        <div className="text-center mt-10">
+          <h1 className="text-[34px] leading-[38.25px] font-bold tracking-[1px] pb-8">
+            Bringing you the <span className="text-[#D87D4A]">best</span> audio
+            gear
+          </h1>
+          <p className="text-[15px] leading-[25px] font-normal w-[80%] m-auto opacity-65">
+            Located at the heart of New York City, Audiophile is the premier
+            store for high end headphones, earphones, speakers, and audio
+            accessories. We have a large showroom and luxury demonstration rooms
+            available for you to browse and experience a wide range of our
+            products. Stop by our store to meet some of the fantastic people who
+            make Audiophile the best place to buy your portable audio equipment.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
